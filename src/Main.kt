3509a55c4262ac670088a11dd63f39bb71fab4c8
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-03-21 07:39:28 +0300

    added files

--- src/Main.kt
+++ src/Main.kt
@@ -7,21 +7,29 @@
 /*(B) Convert the age variable (from question 1) to a String and store it in a new variable named ageAsString.*/
     var ageAsString: String ="25"
      println(ageAsString)
+     On question one I have declared a variable named age and assigned it the value 25,
+     on question 1b I have converted the age variable to a string and stored it in a new variable named ageAsString
+ */
+ */
 
 
 /* 2. Immutable vs. Mutable
 Explain the difference between immutable and mutable variables in Kotlin.*/
-Immutable variables are variables which cannot be changed while mutable variables
-are variables that can be changed
+/*Immutable variables are variables which cannot be changed while mutable variables
+are variables that can be changed*/
 
 /* 3. String Interpolation
 Create a variable named name with your name as its value.
-Then, create a new variable greeting that combines the name variable with a greeting message using string interpolation.*/
+Then, create a new variable greeting that combines the name variable with a greeting message using string interpolation.
 val name = "Faith"
 val  greeting = "Hello"
 val sentence = "$greeting my name is $name"
 println(sentence)
-}*/
+}
+In question 3 I have combined the name variable which I have assigned it the value Faith with the greeting message which I have assigned it to Hello
+using the ($) sign
+ */
+
 /*Function with Parameters
 Write a function named sum that takes two parameters a and b (both integers) and returns their sum.
 */
@@ -35,8 +43,10 @@ fun sum(num1: Int,num2:Int): Int {
     println(sum)
     return sum
 }
+On question 4 I have declared a function named sum that is assigned to the parameters a and b and I have returned its sum
 
 /*Default Parameter Value
+
 Create a function named greet that takes a name parameter (String) and a greeting parameter (String with a default value of "Hello"). The function should return a formatted greeting message.*/
 fun main(){
 val greet = "Hello"
@@ -44,6 +54,9 @@ val greet = "Hello"
 fun greet(name: String ):String{
     println(name)
 }
+In question 5 I have created a function named greet which takes in the name parameter that is a string and the
+default value of the greeting parameter is Hello which returns a formatted greeting message.
+ */
 
 
 
